f25956e4c3905f31e344d83daf5f6ecfad72461b
Angellee <angel.purnamasari@suppapp.com>
2024-12-04 01:31:32 +0800

    feat: helper function to convert string to instant

---
+++ src/main/kotlin/com/example/utils/stringToInstant.kt
@@ -0,0 +1,14 @@
+package com.example.utils
+
+import com.google.protobuf.Timestamp
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
+// Convert Marvel API timestamp string to Instant
+fun String.toInstant(): Instant = Instant.from(DateTimeFormatter.ISO_INSTANT.parse(this))
+
+// Convert Instant to gRPC Timestamp
+fun Instant.toTimestamp(): Timestamp = Timestamp.newBuilder()
+    .setSeconds(epochSecond)
+    .setNanos(nano)
+    .build()
