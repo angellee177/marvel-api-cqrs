d309e01d380e4aea7d9ca49df22cd7639908db95
Angellee <angel.purnamasari@suppapp.com>
2024-12-06 01:38:00 +0800

    refactor: character and cacheCharaters column name

--- src/main/resources/db/migration/V3__create_character_table.sql
+++ src/main/resources/db/migration/V3__create_character_table.sql
@@ -4,6 +4,6 @@ CREATE TABLE Characters (
     name VARCHAR NOT NULL UNIQUE,                         -- Name of the character (e.g., "Spider-Man")
     description TEXT,                              -- Description of the character
     lastModified TIMESTAMP,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the character was created
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp when the character was last updated
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the character was created
+    updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp when the character was last updated
 );
--- src/main/resources/db/migration/V4__create_cache_table.sql
+++ src/main/resources/db/migration/V4__create_cache_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE CacheCharacters (
      id UUID PRIMARY KEY NOT NULL,                  -- Unique identifier for the cache entry
-     cacheKey UUID NOT NULL,                    -- Unique identifier for cache entry
-     data JSONB,                                    -- Cached character data in JSON format
-     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the cache entry was created
-     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Last updated timestamp
-     expires_at TIMESTAMP                         -- Timestamp when the cache entry will expire
+     cacheKey TEXT,                    -- Unique identifier for cache entry
+     data JSONB NOT NULL DEFAULT '[]',                                    -- Cached character data in JSON format
+     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the cache entry was created
+     updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Last updated timestamp
+     expiresAt TIMESTAMP                         -- Timestamp when the cache entry will expire
 );
