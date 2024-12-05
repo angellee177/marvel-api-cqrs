6e77e8f9678075da592224e1605917430b8dfbe0
Angellee <angel.purnamasari@suppapp.com>
2024-12-05 15:46:24 +0800

    test: Marvel Api Client

--- src/test/kotlin/com/example/thirdparty/MarvelApiClientTest.kt
+++ src/test/kotlin/com/example/thirdparty/MarvelApiClientTest.kt
@@ -1,15 +1,30 @@
 package com.example.thirdparty
 
+import io.kotest.matchers.be
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import io.kotest.matchers.*
+import io.kotest.matchers.types.shouldBeTypeOf
 
 class MarvelApiClientTest {
     @Test
     @Tag("adapter")
     fun `can fetch data from marvel APIs`() = runTest {
-        val result = MarvelApiClient().fetchCharacters()
+        val apiResponse = MarvelApiClient().fetchCharacters()
 
-        println(result)
+        apiResponse.results.first().shouldBeTypeOf<MarvelCharacter>()
+    }
+
+    @Test
+    @Tag("adapter")
+    fun `can fetch data with query`() = runTest {
+        val queryParams = mutableMapOf<String, String>()
+        queryParams["name"] = "Hulk"
+
+        val apiResponse = MarvelApiClient().fetchCharacters(queryParams)
+
+        apiResponse.total should be(1)
+        apiResponse.results.first().name shouldBe "Hulk"
     }
 }
